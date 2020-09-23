648d674abe998891558c6865d22a16d1ea314bcc
Makenzie <71519141+MakenzieBuchenroth@users.noreply.github.com>
2020-09-23 16:56:54 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,17 +1,14 @@
-<script language="javascript">
-    function changeImage() {
-
-        if (document.getElementById("imgClickAndChange").src == "placeholder.png") 
+function changeImage() {
+        if (document.getElementById("imgClickAndChange").src == "placeholder.png")
         {
             document.getElementById("imgClickAndChange").src = "placeholder2.png";
         }
-        else if (document.getElementById("imgClickAndChange").src == "placeholder2.png)
+        else if (document.getElementById("imgClickAndChange").src == "placeholder2.png")
         {
           document.getElementById("imgClickAndChange").src = "placeholder3.png";
         }
-        else 
+        else
         {
             document.getElementById("imgClickAndChange").src = "placeholder.png";
         }
     }
-</script>
